9eac3a5e8c3b9dafb45dfb0352727daeec22ec39
mesismart <sabaghy@gmail.com>
2026-06-25 23:43:21 +0330

    ui(SendTicket): add send ticket page

--- src/app/p-admin/account/page.jsx
+++ src/app/p-admin/account/page.jsx
@@ -23,8 +23,47 @@ export default function Account() {
               />
             </div>
           </div>
+          <div className="flex flex-col bg-grey-100 gap-1 mt-4">
+            <label className="text-sm font-bold px-4 ">ایمیل</label>
+            <div className="border-red-900 border-2 rounded-md px-4 py-2 ">
+              <input
+                placeholder="لطفا ایمیل خود را وارد کنید "
+                className="outline-none border-0 align-right w-full"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col bg-grey-100 gap-1 mt-4">
+            <label className="text-sm font-bold px-4 ">شماره تماس</label>
+            <div className="border-red-900 border-2 rounded-md px-4 py-2 ">
+              <input
+                placeholder="لطفا شماره تماس خود را وارد کنید "
+                className="outline-none border-0 align-right w-full"
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex-1"></div>
+        <div className="flex-1 px-4">
+          <div></div>
+          <div className="flex flex-col bg-grey-100 gap-1 mt-4">
+            <label className="text-sm font-bold px-4 ">رمز عبور</label>
+            <div className="flex gap-2 items-stretch">
+              <div className="flex-1 border-red-900 border-2 rounded-md px-4 py-2 ">
+                <input
+                  placeholder="لطفا رمز عبور خود را وارد کنید "
+                  className="outline-none border-0 align-right w-full"
+                />
+              </div>
+              <button className="bg-red-900  text-white px-4 py-2 rounded-md whitespace-nowrap">
+                تغییر رمز عبور
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center gap-4 mt-20">
+        <button className="bg-red-900 w-60 text-white px-4 py-2 rounded-md">
+          ثبت تغییرات
+        </button>
       </div>
       <div></div>
     </PanelUserLayout>
---
+++ src/app/p-admin/tickets/sendTicket/page.jsx
@@ -0,0 +1,55 @@
+import PanelUserLayout from "@/components/layouts/PanelUserLayout";
+
+export default function SendTicket() {
+  return (
+    <PanelUserLayout>
+      <div className="flex items-center gap-4 mt-4">
+        <hr className="flex-1 border-t-2 border-red-900" />
+        <h1 className="whitespace-nowrap m-0 text-2xl font-bold">
+          ارسال تیکت جدید
+        </h1>
+        <hr className="flex-[12] border-t-2 border-red-900" />
+      </div>
+
+      <div className=" gap-4 mt-4 w-full">
+        <div className="flex px-4  mt-4 justify-between gap-4">
+          <div className="flex flex-col w-full bg-grey-100 gap-1 mt-4">
+            <label className="text-sm font-bold px-4 ">نام کاربری</label>
+            <div className="border-red-900 border-2 rounded-md px-4 py-2 ">
+              <select
+                defaultValue=""
+                className="outline-none border-0 bg-transparent w-full"
+              >
+                <option value="" disabled>
+                  دپارتمان خود را انتخاب کنید
+                </option>
+                <option value="support">پشتیبانی</option>
+                <option value="sales">فروش</option>
+                <option value="billing">حسابداری</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full bg-grey-100 gap-1 mt-4">
+            <label className="text-sm font-bold px-4 ">
+              دپارتمان خود را انتخاب کنید
+            </label>
+            <div className="border-red-900 border-2 rounded-md px-4 py-2 ">
+              <select
+                defaultValue=""
+                className="outline-none border-0 bg-transparent w-full"
+              >
+                <option value="" disabled>
+                  دپارتمان خود را انتخاب کنید
+                </option>
+                <option value="support">پشتیبانی</option>
+                <option value="sales">فروش</option>
+                <option value="billing">حسابداری</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </PanelUserLayout>
+  );
+}
